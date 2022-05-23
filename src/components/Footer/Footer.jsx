a808577fa79c9835b0b5806f7bee6c3fe69f3568
Facu <84508942+faqbb@users.noreply.github.com>
2022-05-23 17:27:06 -0300

    desafio counter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import fonts from '../fonts/fonts.css'
 function Footer() {
   return (
-    <div className='row justify-content-around bg-dark fixed-bottom position-relative'>
+    <div className='row justify-content-around bg-dark bottom-0'>
         <div>
             <ul className='d-flex m-0'>
                 <li className='px-3 pt-3'><img src='../src/components/imgs/wpp.png'className='img-fluid'></img></li>
